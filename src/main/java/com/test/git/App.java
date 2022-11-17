b47aa1ef1fdfecfc3fb87c07cff22c6ec399dd5a
lizhiwei <li-zw@primoton.com>
2022-11-17 18:23:13 +0800

    第三次提交哈哈哈哈

--- src/main/java/com/test/git/App.java
+++ src/main/java/com/test/git/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main( String[] args ) {
         System.out.println("第一次修改!");
         System.out.println("第二次修改!");
+        System.out.println("第三次修改,哈哈哈哈哈哈哈哈哈!");
         System.out.println( "Hello World!" );
     }
 }
