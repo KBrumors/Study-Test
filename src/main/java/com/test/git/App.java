3760c8946afa3fcd4efb1d9a92a83e613bc6d479
lizhiwei <li-zw@primoton.com>
2022-11-17 17:05:53 +0800

    第一次提交app.java

---
+++ src/main/java/com/test/git/App.java
@@ -0,0 +1,12 @@
+package com.test.git;
+
+/**
+ * Hello world!
+ *
+ * @author 李志伟
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
