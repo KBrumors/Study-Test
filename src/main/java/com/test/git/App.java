770f7c7e268ac4eb4d309e831da71d40386cbf9a
lizhiwei <li-zw@primoton.com>
2022-11-17 17:45:18 +0800

    第一次修改

--- src/main/java/com/test/git/App.java
+++ src/main/java/com/test/git/App.java
@@ -7,6 +7,7 @@ package com.test.git;
  */
 public class App {
     public static void main( String[] args ) {
+        System.out.println("第一次修改!");
         System.out.println( "Hello World!" );
     }
 }
