3724464f2db61966bae64079b7aca6bd1caf0741
lizhiwei <li-zw@primoton.com>
2022-11-18 20:51:07 +0800

    在develop分支上继续工作

--- src/main/java/com/test/git/Custom.java
+++ src/main/java/com/test/git/Custom.java
@@ -7,5 +7,6 @@ public class Custom {
 	public static void main(String[] args) {
 		System.out.println("这可是custom的输出");
 		System.out.println("在develop分支上开始工作吧");
+		System.out.println("在develop分支上继续工作");
 	}
 }
